a2eff654fb62af2ace73956be4d003910eab5366
naseef786 <naseefac786@gmail.com>
2025-11-27 15:47:02 +0400

    Hot Fixes

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app"); // if you have a separate server.js file
-const serverless = require("serverless-http");
-
-module.exports = (req, res) => {
-  const handler = serverless(app);
-  return handler(req, res);
-};
